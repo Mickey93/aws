7385bd8f6e96b787cfa7dd6a038439b7b3cc36c8
mickey <michael.redaelli@gmail.com>
2024-06-16 00:20:48 +0100

    updated to conform to pep8

--- auth/sessions.py
+++ auth/sessions.py
@@ -1,12 +1,16 @@
 from os import getenv
 from boto3 import Session as Boto3Session
 
+
 class CustomSession:
+
+    def __init__(self):
+        pass
 
     @staticmethod
     def aws_session() -> object:
         return Boto3Session(
-            aws_access_key_id = getenv('ACCESS_KEY'),
-            aws_secret_access_key = getenv('SECRET_KEY'),
+            aws_access_key_id=getenv('ACCESS_KEY'),
+            aws_secret_access_key=getenv('SECRET_KEY'),
             region_name=getenv('REGION')
         )
